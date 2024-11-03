540909f70a89383afedd951e173236ed2fe4b6cb
Khambazarov <khambazarovrenat@gmail.com>
2024-11-03 14:52:42 +0100

    add feat: Scan QR-Code to Home.tsx

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -4,13 +4,18 @@ export function Home() {
   return (
     <div>
       <h1>Welcome to Our Restaurant</h1>
-<p>Enjoy the best dishes in town!</p>
-<img
-  src="https://images.pexels.com/photos/1055058/pexels-photo-1055058.jpeg?auto=compress&cs=tinysrgb&w=320&dpr=1"
-  alt="Restaurant"
-/>
-<section>
-  <h2>Our Specialties</h2>
+      <p>Enjoy the best dishes in town!</p>
+      <img
+        src="https://images.pexels.com/photos/1055058/pexels-photo-1055058.jpeg?auto=compress&cs=tinysrgb&w=320&dpr=1"
+        alt="Restaurant"
+      />
+      {/* https://goqr.me/de/ => Hinweis: Der QR Code kann komplett kostenlos verwendet werden (kommerzielle Nutzung und Druck erlaubt). */}
+      <img
+        src="https://api.qrserver.com/v1/create-qr-code/?color=000000&amp;bgcolor=FFFFFF&amp;data=https%3A%2F%2Ffinal-project-pizza.netlify.app%2F&amp;qzone=0&amp;margin=5&amp;size=100x100&amp;ecc=L"
+        alt="qr code"
+      />
+      <section>
+        <h2>Our Specialties</h2>
         <ul>
           <li>Pizza Margherita</li>
           <li>Grilled Salmon</li>
